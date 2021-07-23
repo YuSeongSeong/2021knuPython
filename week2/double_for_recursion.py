ba825e0a0a419002c8573c374d70db1ee2034f02
YuSeongSeong <48575834+YuSeongSeong@users.noreply.github.com>
2021-07-23 16:56:05 +0900

    second week

---
+++ week2/double_for_recursion.py
@@ -0,0 +1,5 @@
+# for 문 속 for 문 / 이중 for 문
+for i in range(10):
+    for j in range(1,11):
+        print(j,end=" ")
+    print()
---
+++ week2/for_recursion.py
@@ -0,0 +1,30 @@
+# for문을 무작정 따라해보기
+for i in range(1, 11):
+    if(i%2 == 0):
+        print(i,"는 짝수입니다.")
+    else:
+        print(i,"는 홀수입니다.")
+    
+
+# for 문의 구조
+# for i in 범위:
+# 반복할 명령어1
+# 반복할 명령어2
+
+#for문 with list
+mylist = ['해달이', '사스미', '메기']
+for i in mylist:
+    print(i)
+print("반복 끝")
+
+
+# range list
+print(list(range(1,11)))
+print(list(range(10))) #시작점 기본값은 0
+print(list(range(1,20,3)))
+print(list(range(20,0,-3)))
+
+# for문 with range
+for i in range(1,11):
+    print(i, end=" ")
+print('반복 끝')
