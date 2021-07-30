9edc073d5358909d1f4b75ca5eacce830d2c024a
YuSeongSeong <48575834+YuSeongSeong@users.noreply.github.com>
2021-07-30 16:36:16 +0900

    week3-2

---
+++ week3-2/multiple_arauments.py
@@ -0,0 +1,8 @@
+def people_num(*args):
+    print("공부하는 학생 수는?: ", end = "")
+    print(args)
+    result = len(args)
+    return result
+
+value = people_num("해달이", "사스미", "시라용", "부기", "메기")
+print(value,"명")
---
+++ week3-2/parameter+argument_exercise.py
@@ -0,0 +1,23 @@
+# 인수의 순서를 바꿔보기
+def add_two_num(a=100,b=200):
+    print("덧셈중...")
+    print(a,b)
+    result = a + b
+    return result
+
+value1 = add_two_num(10,13)
+value2 = add_two_num(10)
+value3 = add_two_num(b = 10)
+value4 = add_two_num()
+
+print(value1,value2,value3,value4)
+
+
+# 가장 뒤의 변수부터 기본값 지정
+def add_two_num(a=100,b): #불가능
+    ...
+
+def add_two_num(a,b=100): #가능
+    ...
+
+    
